7add327be94559fa0042d24e21bb7dd14cf816d1
ralphschaur9 <ralphschaur9@gmail.com>
2025-09-08 08:54:17 +0000

    [Add] check if input contains data

--- Lektion-01/Klient/index.js
+++ Lektion-01/Klient/index.js
@@ -1,1 +1,22 @@
-const form = document.querySelector(".input-form")
+const form = document.querySelector(".input-form") // Läs in formulär
+const submitBtn = document.querySelector("button[type='submit']")// Läs in submit knapp
+
+let inputName = form.elements.name.value // Läser in det som skrivs i namn input
+let inputMessage = form.elements.message.value // Läser in det som skrivs i message
+
+const checkInputs = () => {
+    inputName = form.elements.name.value
+    inputMessage = form.elements.message.value
+
+    if (!inputName || !inputMessage) submitBtn.disabled = true 
+    else submitBtn.disabled = false
+}
+
+form.addEventListener("input", checkInputs())
+
+form.addEventListener("submit", async (e) =>{ // e en förkortning för event
+  e.preventDefault() //Hindrar formuläret från att ladda om 
+
+    if (!inputName || !inputMessage) return alert("Fyll i båda fälten")
+        
+})
